5cf5bde98a8cdc8c0ffed6b552daee2177ca33ea
Kássio Silva <kassio.slva@gmail.com>
2019-02-05 19:13:15 -0300

    Add criptografia de senha

--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,9 @@ const express = require('express')
 
 const routes = express.Router()
 
+const UserController = require('./app/controllers/UserController')
+
+// Rota para a criação dos usuários
+routes.post('/users', UserController.store)
+
 module.exports = routes
